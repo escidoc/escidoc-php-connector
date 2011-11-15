eacab7c1e5a3721e2f9344ad2502e29eeb233f7c
mk0 <marko.voss@fiz-karlsruhe.de>
2011-11-15 14:05:02 +0000

    Adding Exceptions as files for classloading.

---
+++ src/client/exceptions/server/application/invalid/ValidationException.php
@@ -0,0 +1,7 @@
+<?php
+namespace escidoc\client\exceptions\server\application\invalid;
+
+use escidoc\client\exceptions\server\application\ApplicationException;
+
+class ValidationException extends ApplicationException {}
+?>
